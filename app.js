01b1c24c5e92086370a7d5c68bd7b71751915f54
unknown <tronn232003@gmail.com>
2022-06-28 16:08:42 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+//express app
+const app = express();
+
+//conncet to mongodb
+const dbURI = 'mongodb+srv://Tron:Tronn62158@cluster0.p45b29s.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  // .then((result) => console.log('connected to db'))
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// register view engine
+app.set('view engine', 'ejs');
+
+//middleware and static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+
+// routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
+
+
+
+app.get('/about', (req, res) =>{
+    // res.send('<p>about page</p>');
+    res.render('about', { title: 'about' });
+})
+
+app.use('/blogs', blogRoutes);
+
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+});
